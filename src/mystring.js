d3d25ea9afc23f31a66674c8b2c9e8a2f2ad0ae0
8Bts <mcmizze@yahoo.com>
2021-03-25 20:01:25 +0400

    Add test suite for mystring.js

---
+++ src/mystring.js
@@ -0,0 +1,7 @@
+const mystring = (() => {
+  const capitalize = (str) => {};
+
+  return { capitalize };
+})();
+
+export default mystring;
---
+++ tests/mystring.test.js
@@ -0,0 +1,10 @@
+import { expect, test } from '@jest/globals';
+import mystring from '../src/mystring';
+
+test('Returns capitalized string if first character is lowercase and alphabetic', () => {
+  expect(mystring.capitalize('my first day at university')).toEqual('My first day at university');
+});
+
+test('Returns same string if first character is uppercase or non-alphabetic', () => {
+  expect(mystring.capitalize('1. create repo')).toEqual('1. create repo');
+});
